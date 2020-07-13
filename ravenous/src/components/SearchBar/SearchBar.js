2e7e80e9067785159355b43e46a13e7e880b69f3
georgiev-ventsi <v.g.al.hq@gmail.com>
2020-07-13 10:31:18 +0300

    Handle a Term or Location Change

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -8,35 +8,46 @@ const sortByOptions = {
 }
 
 class SearchBar extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             term: '',
             location: '',
             sortBy: 'best_match'
         };
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+
     }
 
-    getSortByClass(sortByOption){
-        if(sortByOption === this.state.sortBy){
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    handleLocationChange(event) {
+        this.setState({ location: event.target.value });
+    }
+
+    getSortByClass(sortByOption) {
+        if (sortByOption === this.state.sortBy) {
             return 'active';
         } else {
             return '';
         }
     }
 
-    handleSortBychange(sortByOption){
+    handleSortBychange(sortByOption) {
         this.setState({ sortBy: sortByOption });
     }
 
-    renderSortByOptions(){
+    renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}> {sortByOption} </li>;
+            return <li key={sortByOptionValue} onClick={this.handleSortBychange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} > {sortByOption} </li>;
         })
     }
 
-    render(){
+    render() {
         return (
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
@@ -45,15 +56,15 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                    <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
                 </div>
             </div>
-            )
-        }
+        )
     }
+}
 
 export default SearchBar;
